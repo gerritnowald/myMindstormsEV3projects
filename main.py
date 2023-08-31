f9fa6828126450d963ee38206beae1c4d80183e0
Gerrit Nowald <gerrit.nowald@gmail.com>
2023-08-31 21:52:17 +0200

    validated robot dimensions

--- main.py
+++ main.py
@@ -1,30 +1,29 @@
 #!/usr/bin/env pybricks-micropython
 from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
-                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+                                 InfraredSensor, GyroSensor)
 from pybricks.parameters import Port, Stop, Direction, Button, Color
-from pybricks.tools import wait, StopWatch, DataLog
+# from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
-from pybricks.media.ev3dev import SoundFile, ImageFile
+# from pybricks.media.ev3dev import SoundFile, ImageFile
 
 
-# This program requires LEGO EV3 MicroPython v2.0 or higher.
-# Click "Open user guide" on the EV3 extension tab for more information.
-
-
-# Create your objects here.
 ev3 = EV3Brick()
 
-
-# Write your program here.
-ev3.speaker.beep()
-
 # Initialize the motors.
-left_motor = Motor(Port.B)
+left_motor  = Motor(Port.B)
 right_motor = Motor(Port.C)
 
 # Initialize the drive base.
-robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
+robot = DriveBase(left_motor, right_motor, wheel_diameter=62.4, axle_track=118)
 
-# Go forward and backwards for one meter.
-robot.straight(100)
+
+robot.straight(1000)
+
+robot.turn(180)
+
+robot.straight(800)
+
+robot.turn(180)
+
+robot.straight(-200)
