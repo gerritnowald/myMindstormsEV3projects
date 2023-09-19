ba19f0473316807634581f2144c099daeef38eef
Gerrit Nowald <gerrit.nowald@gmail.com>
2023-09-19 21:22:21 +0200

    drive with random turn at obstacle

--- main.py
+++ main.py
@@ -3,30 +3,39 @@ from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
                                  InfraredSensor, GyroSensor)
 from pybricks.parameters import Port, Stop, Direction, Button, Color
-# from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 # from pybricks.media.ev3dev import SoundFile, ImageFile
 
+import random
+
+#------------------------------------------------------------------------------
+# user input
+
+speed = 100   # mm/s 
+
+#------------------------------------------------------------------------------
+# initialization
 
 ev3 = EV3Brick()
 
-# Initialize the motors.
 left_motor  = Motor(Port.B)
 right_motor = Motor(Port.C)
 
-# Initialize the drive base.
-robot = DriveBase(left_motor, right_motor, wheel_diameter=62.4, axle_track=110)
+obstacle_sensor = InfraredSensor(Port.S4)
+bumper          = TouchSensor(Port.S3)
 
+robot = DriveBase(left_motor, right_motor, wheel_diameter = 62.4, axle_track = 110)
+robot.settings(speed, straight_acceleration = 100, turn_rate = 100, turn_acceleration = 100)
 
-# robot.turn(360)
+#------------------------------------------------------------------------------
+# main loop
 
+while True:
+    robot.drive(speed, 0)
 
-robot.straight(1000)
-
-robot.turn(180)
-
-robot.straight(800)
-
-robot.turn(180)
-
-robot.straight(-200)
+    while obstacle_sensor.distance() > 20 and bumper.pressed() is not True:
+        wait(100)    # ms
+    
+    robot.straight(-50)     # mm
+    robot.turn(random.randint(30,180))  # °
