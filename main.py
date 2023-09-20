93dc4bdbe1939e78e9caee2ebd9bfa0ce89ad48b
Gerrit Nowald <gerrit.nowald@gmail.com>
2023-09-20 06:12:31 +0200

    path logging on EV3

--- main.py
+++ main.py
@@ -28,6 +28,9 @@ bumper          = TouchSensor(Port.S3)
 robot = DriveBase(left_motor, right_motor, wheel_diameter = 62.4, axle_track = 110)
 robot.settings(speed, straight_acceleration = 100, turn_rate = 100, turn_acceleration = 100)
 
+data  = DataLog('time / s', 'distance / mm', 'clockwise rotation / °', name='position', timestamp=False)
+watch = StopWatch()
+
 #------------------------------------------------------------------------------
 # main loop
 
@@ -38,4 +41,9 @@ while True:
         wait(100)    # ms
     
     robot.straight(-50)     # mm
+    data.log(watch.time()/1000, robot.distance(), 0)
+
     robot.turn(random.randint(30,180))  # °
+    data.log(watch.time()/1000, 0, robot.angle())
+
+    robot.reset()   # resets measurement
